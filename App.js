f773ad043177590011afe025c7a9411ee20b1895
Trace Brandt <trpbr92@gmail.com>
2021-09-08 10:20:13 -0500

    fonts

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
 
-import CategoriesScreen from './screens/CategoriesScreen';
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.tff')
+  });
+} 
+
 
 export default function App() {
+const [fontLoaded, setFontLoaded] = useState(false);
+
+if (!fontLoaded) {
+  return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+}
+
   return (
     <View>
       <CategoriesScreen />
