1571f056d6864ccfd3e6a54af528e1fa743b59e9
Trace Brandt <trpbr92@gmail.com>
2021-09-14 11:12:38 -0500

    dummy data displaying on grid in catergories screen

---
+++ data/dummy-data.js
@@ -0,0 +1,16 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+
+new Category('c1', 'Italian', '#f5428d'),
+new Category('c2', 'Quick & Easy', '#f54242'),
+new Category('c3', 'Burgers', '#f5a442'),
+new Category('c4', 'Thai', '#fd142'),
+new Category('c5', 'Mexican', '#368dff'),
+new Category('c6', 'Breakfast', '#41d95d'),
+new Category('c7', 'French', '#9eecff'),
+new Category('c8', 'Chinese', '#b9ffb0'),
+new Category('c9', 'Salads', '#ffc7ff'),
+new Category('c10', 'Sauces', '#47fced'),
+
+]
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet} from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+    return ( 
+    <View style={styles.gridItems}>
+        <Text>{itemData.item.title}</Text>
+    </View>
+    )
+};
 
 const CategoriesScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to Meals!" onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals'});
-            }} />
-        </View>
-    );
+<FlatList 
+    data={CATEGORIES} 
+    renderItem={renderGridItem} 
+    numColumns={2} />
+  );
 }
 
 const styles = StyleSheet.create({
@@ -17,6 +25,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItems: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
