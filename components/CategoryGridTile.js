f3e0e6c99434ef3c20c87d4807f684dd2e24ec2f
Trace Brandt <trpbr92@gmail.com>
2021-09-21 10:20:57 -0500

    grid styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -3,12 +3,11 @@ import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import { ScreenContainer } from 'react-native-screens';
 
 const CategoryGridTile = props => {
+
     return (
-        <TouchableOpacity 
-        style={styles.gridItems}
-        onPress={()=>  {props.onSelect}}>
+        <TouchableOpacity style={styles.gridItems} onPress={props.onSelect}>
 <View style={{ ...styles.container, ...{backgroundColor: props.color}}}>
-    <Text>{props.title}</Text>
+    <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
 </View>
 </TouchableOpacity>
     );
@@ -21,7 +20,20 @@ const styles = StyleSheet.create({
         height: 150
     },
     container: {
-        flex: 1
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    title: {
+        fontSize: 22,
+        textAlign: 'right'
     }
 })
 
