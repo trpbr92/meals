fd6cbc8bba578d45fb24a4a1d0bef82946d5a81b
Trace Brandt <trpbr92@gmail.com>
2021-09-29 08:34:57 -0500

    MealItem

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
     const renderMealItem = itemData => {
-        return (<View><Text>{itemData.item.title}</Text></View>);
+        return (<MealItem title={itemData.item.title} onSelect={() => {}} />);
     }
 
    const catId = props.navigation.getParam('categoryId');
@@ -14,7 +15,7 @@ const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0
 
     return (
         <View style={styles.screen}>
-            <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem} />
+            <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem} style={{width: '100%'}} />
         </View>
     );
 };
